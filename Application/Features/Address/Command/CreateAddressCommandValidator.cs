34025e194ce22ad62517b85452ada8e625c18ccf
Chowdhury M Rahman <cmrahman@gmail.com>
2021-04-20 14:45:05 +1000

    Azure validation for AddAddress

--- Application/Features/Address/Command/CreateAddressCommandValidator.cs
+++ Application/Features/Address/Command/CreateAddressCommandValidator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentValidation;
 
 namespace Application.Features.Address.Command
@@ -15,21 +18,49 @@ namespace Application.Features.Address.Command
                 .NotNull();
 
             RuleFor(e => e)
-                .Must(IsValidStateAndPostCode)
+                .MustAsync(StateAndPostCodeValid)
                 .WithMessage("Please provide a valid state and postcode");
 
 
         }
 
-        private bool IsValidStateAndPostCode(CreateAddressCommand arg)
+        private async Task<bool> StateAndPostCodeValid(CreateAddressCommand arg, CancellationToken token)
         {
-            return _states.Any(str => str.Contains(arg.State))
-                && _postCodes.Any(codes => codes.Equals(arg.PostCode));
+            //TODO : Change validation to use real data instead of static List of States & Codes
+
+            /**********************************************************************************
+             *
+            Currently this method calls an Azure Function for validation.
+            Validation is being done against a static list of States and PostCodes.
+
+            Valid States and PostCode lists >>
+
+            List<string> states = new List<string> { "NSW", "ACT", "QLD", "VIC", "SA" };
+            List<int> postCodes = Enumerable.Range(2500, 1000).ToList();
+
+            **********************************************************************************/
+
+            bool isValid = false;
+
+            using (var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://validateaddress.azurewebsites.net")
+            })
+            {
+                var response = await httpClient.GetAsync($"api/ValidateAddress?state={arg.State}&postcode={arg.PostCode}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string resultContent = await response.Content.ReadAsStringAsync(token);
+                    isValid = resultContent.Contains("valid");
+
+                }
+            }
+
+            return await Task.FromResult(isValid);
         }
 
-
-        private readonly List<string> _states = new List<string> {"NSW", "ACT", "QLD", "VIC", "SA"};
-        private readonly List<int> _postCodes = Enumerable.Range(2500, 1000).ToList();
+       
 
     }
 }
